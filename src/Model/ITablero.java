61dc38f49a6f13f98472ba2504631db4cf073c42
Robinson Castro Fern�ndez <r.a.c.f.28@gmail.com>
2016-11-28 18:52:43 -0300

    tablero

--- src/Model/ITablero.java
+++ src/Model/ITablero.java
@@ -4,4 +4,6 @@ public interface ITablero {
   boolean contains(Point point);
   
   void setColor(Point point, Color color);
+  
+  Color getColor(Point point);
 }
---
+++ src/Model/Tablero.java
@@ -0,0 +1,33 @@
+package Model;
+
+import java.util.Arrays;
+
+public class Tablero implements ITablero {
+  private Color[][] colorOf;
+  private int width;
+  private int height;
+  
+  public Tablero(int width, int height, Color defaultColor) {
+    this.width = width;
+    this.height = height;
+    colorOf = new Color[width][height];
+    for (Color[] row : colorOf)
+      Arrays.fill(row, defaultColor);
+  }
+  
+  @Override
+  public boolean contains(Point point) {
+    return 0 <= point.x && point.x < width
+        && 0 <= point.y && point.y < height;
+  }
+
+  @Override
+  public void setColor(Point point, Color color) {
+    colorOf[point.x][point.y] = color;
+  }
+
+  @Override
+  public Color getColor(Point point) {
+    return colorOf[point.x][point.y];
+  }
+}
